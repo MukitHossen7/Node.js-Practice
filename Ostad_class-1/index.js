03e83ab637ac2970d1ca4a3ae0dc46c162c573a3
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-14 16:41:47 +0600

    Initialize server setup and add author information in package.json

---
+++ Ostad_class-1/index.js
@@ -0,0 +1,11 @@
+import http from "http";
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello World");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
