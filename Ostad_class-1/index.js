e5027c7c1b73f22a8b35fb45b5bf734a182fdb5c
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-16 15:43:56 +0600

    Comment out server setup code and log global objects

--- Ostad_class-1/index.js
+++ Ostad_class-1/index.js
@@ -1,11 +1,14 @@
 // import http from "http";
-const http = require("http");
-const port = 3000;
+// const http = require("http");
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.end("Hello World");
-});
+// const server = http.createServer((req, res) => {
+//   res.end("Hello World");
+// });
 
-server.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// server.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+// console.log(global);
+global.console.log("Hello World");
+console.log(globalThis);
