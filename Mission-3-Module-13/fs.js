ed051b3c4a34c82e759590f625b86d7819b390bf
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-30 00:13:17 +0600

    Add console logs to fs.js for task tracking during file operations

--- Mission-3-Module-13/fs.js
+++ Mission-3-Module-13/fs.js
@@ -1,8 +1,12 @@
 const fs = require("fs");
 
+console.log("task 1");
 const dream = "My Dream is a DSA Developer";
 
+console.log("task 2");
 fs.writeFileSync("text.txt", dream);
 
+console.log("task 3");
 const data = fs.readFileSync("text.txt", { encoding: "utf-8" });
 console.log(data);
+console.log("task 4");
