47690b48efd195282b533f6dfe574394c5e7ec82
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-30 23:20:23 +0600

    Refactor HTTP server to handle GET and POST requests for adding todos

--- Mission-3-Module-13/Todo_App/index.js
+++ Mission-3-Module-13/Todo_App/index.js
@@ -1,7 +1,37 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello Javascript Developer");
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      res.end("Hello Java");
+    } else if (req.url === "/get-todo") {
+      res.end("get todo data");
+    }
+  }
+  if (req.method === "POST") {
+    if (req.url === "/add-todo") {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        console.log("Received body:", body);
+
+        // Try to parse JSON if applicable
+        try {
+          const data = JSON.parse(body);
+          console.log("Parsed JSON:", data);
+
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ message: "Data received", received: data }));
+        } catch (err) {
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ error: "Invalid JSON" }));
+        }
+      });
+    }
+  }
 });
 server.listen(3000, () => {
   console.log("The server is running");
