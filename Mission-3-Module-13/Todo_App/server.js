e68b69f69695f095fa1ce71b43388848d5439dca
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-31 00:34:40 +0600

    Refactor server.js to return JSON data for GET requests on /todos

--- Mission-3-Module-13/Todo_App/server.js
+++ Mission-3-Module-13/Todo_App/server.js
@@ -1,8 +1,49 @@
 const http = require("http");
 
+const data = [
+  {
+    id: 1,
+    title: "Complete React assignment",
+    description: "Finish the components and submit by tonight",
+    completed: false,
+    dueDate: "2025-06-01",
+  },
+  {
+    id: 2,
+    title: "Call mentor",
+    description: "Discuss project progress and feedback",
+    completed: true,
+    dueDate: "2025-05-28",
+  },
+  {
+    id: 3,
+    title: "Update portfolio website",
+    description: "Add latest project and fix broken links",
+    completed: false,
+    dueDate: "2025-06-05",
+  },
+  {
+    id: 4,
+    title: "Buy groceries",
+    description: "Milk, eggs, bread, and vegetables",
+    completed: true,
+    dueDate: "2025-05-29",
+  },
+  {
+    id: 5,
+    title: "Read 1 chapter of JavaScript book",
+    description: "Focus on closures and scope",
+    completed: false,
+    dueDate: "2025-05-31",
+  },
+];
+
 const server = http.createServer((req, res) => {
   if (req.url === "/todos" && req.method === "GET") {
-    res.end("All GET Todo DATA");
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(JSON.stringify(data));
   } else if (req.url === "/todos/create" && req.method === "POST") {
     res.end("Post the Todo DATA");
   } else {
