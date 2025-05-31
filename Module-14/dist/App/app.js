ebb2cb0146058771bc160765d2adfc26137c249d
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-31 23:50:31 +0600

    Add todo.json and implement GET and POST endpoints for todos in app.js

--- Module-14/dist/App/app.js
+++ Module-14/dist/App/app.js
@@ -4,8 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+const filePath = path_1.default.join(__dirname, "../../db/todo.json");
+//middleware for user send data use this
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
-    res.send("Hello New Express Developer");
+    res.send("Hello New Express Developer !!!");
+});
+//get all todos data
+app.get("/todos", (req, res) => {
+    const todo_data = fs_1.default.readFileSync(filePath, { encoding: "utf8" });
+    res.json(JSON.parse(todo_data));
+});
+//post single todo data
+app.post("/todos/create", (req, res) => {
+    const todoData = req.body;
+    console.log(todoData);
+    res.send("ADD a single Todo Data");
 });
 exports.default = app;
--- Module-14/src/App/app.ts
+++ Module-14/src/App/app.ts
@@ -1,9 +1,27 @@
 import express, { Application, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 
 const app: Application = express();
+const filePath = path.join(__dirname, "../../db/todo.json");
+
+//middleware for user send data use this
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello New Express Developer");
+  res.send("Hello New Express Developer !!!");
 });
 
+//get all todos data
+app.get("/todos", (req: Request, res: Response) => {
+  const todo_data = fs.readFileSync(filePath, { encoding: "utf8" });
+  res.json(JSON.parse(todo_data));
+});
+
+//post single todo data
+app.post("/todos/create", (req: Request, res: Response) => {
+  const todoData = req.body;
+  console.log(todoData);
+  res.send("ADD a single Todo Data");
+});
 export default app;
