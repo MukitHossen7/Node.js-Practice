b89cc62c329151aa5165c1e94af2387c2dac587c
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-31 10:28:50 +0600

    Fix 404 response header for unknown routes in server.js

--- Mission-3-Module-13/Todo_App/server.js
+++ Mission-3-Module-13/Todo_App/server.js
@@ -18,6 +18,7 @@ const server = http.createServer((req, res) => {
   else if (req.url === "/todos/create" && req.method === "POST") {
     res.end("Post the Todo DATA");
   } else {
+    res.writeHead(404, { "content-type": "text/plain" });
     res.end("Your res Not Found");
   }
 });
