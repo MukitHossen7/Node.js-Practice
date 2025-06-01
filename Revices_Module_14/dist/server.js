e82b70e353d7c89ee30b735f75db2a8a225c5fe9
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-01 15:15:52 +0600

    Implement MongoDB connection in server and update package dependencies

--- Revices_Module_14/dist/server.js
+++ Revices_Module_14/dist/server.js
@@ -1,13 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongodb_1 = require("mongodb");
 const app_1 = __importDefault(require("./app"));
 const port = 5000;
-const serverConnection = () => {
+const uri = "mongodb://localhost:27017/todosDB";
+const client = new mongodb_1.MongoClient(uri, {
+    serverApi: {
+        version: mongodb_1.ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+const serverConnection = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield client.connect();
+    yield client.db("todosDB").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
     app_1.default.listen(port, () => {
         console.log(`Server is listening on port ${port}`);
     });
-};
+});
 serverConnection();
--- Revices_Module_14/src/server.ts
+++ Revices_Module_14/src/server.ts
@@ -1,8 +1,21 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
 import app from "./app";
 
 const port = 5000;
 
-const serverConnection = () => {
+const uri = "mongodb://localhost:27017/todosDB";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const serverConnection = async () => {
+  await client.connect();
+  await client.db("todosDB").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
   app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
   });
