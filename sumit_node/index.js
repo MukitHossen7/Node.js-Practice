13a776f1c7d9b61576b21419cbe9f0ad74ee10f9
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-01 23:22:28 +0600

    Add initial implementation of path module usage to log file details

---
+++ sumit_node/index.js
@@ -0,0 +1,6 @@
+const path = require("path");
+
+const pathName = "E:/Node.js-Practice/sumit_node/index.js";
+console.log(path.basename(pathName));
+console.log(path.dirname(pathName));
+console.log(path.extname(pathName));
