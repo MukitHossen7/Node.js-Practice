229961a95b9d027a06c4068c55fc10f2940c271a
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-01 23:39:56 +0600

    Refactor index.js to comment out unused path and os module logs; create text.txt with initial content

--- sumit_node/index.js
+++ sumit_node/index.js
@@ -1,6 +1,16 @@
 const path = require("path");
-
+const os = require("os");
+const fs = require("fs");
 const pathName = "E:/Node.js-Practice/sumit_node/index.js";
-console.log(path.basename(pathName));
-console.log(path.dirname(pathName));
-console.log(path.extname(pathName));
+// console.log(path.basename(pathName));
+// console.log(path.dirname(pathName));
+// console.log(path.extname(pathName));
+// console.log(path.parse(pathName));
+
+// console.log(os.platform());
+// console.log(os.homedir());
+// console.log(os.freemem());
+// console.log(os.cpus());
+
+// fs.writeFileSync("text.txt", "Hello Developer");
+fs.appendFileSync("text.txt", "Hello Mukit \n");
