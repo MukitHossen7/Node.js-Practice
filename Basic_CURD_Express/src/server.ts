d70bf720c89e0d1b9bd7a52c2adf33243c72a8bf
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-10 00:25:33 +0600

    Implement initial Express server with basic CRUD operation endpoint

--- Basic_CURD_Express/src/server.ts
+++ Basic_CURD_Express/src/server.ts
@@ -2,3 +2,14 @@ import express, { Application } from "express";
 
 const app: Application = express();
 const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("I do CRUD operation");
+});
+
+const serverConnection = async () => {
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+};
+serverConnection();
