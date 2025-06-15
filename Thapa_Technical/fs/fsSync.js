d9f1c067e6d4804477016b5935fbcbdae77fcc15
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-15 23:33:28 +0600

    Refactor file handling in fsSync.js and update test.txt content for clarity

--- Thapa_Technical/fs/fsSync.js
+++ Thapa_Technical/fs/fsSync.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
 
-fs.writeFileSync("example.txt", "Hello, World! 2", { encoding: "utf8" });
-// fs.appendFileSync("test.txt", "Hello, World! 2", { encoding: "utf8" });
+// fs.writeFileSync("example.txt", "Hello, World! 2", { encoding: "utf8" });
+// fs.appendFileSync("test.txt", "\nHello, World! 3", { encoding: "utf8" });
+// fs.renameSync("example.txt", "example2.txt");
+fs.unlinkSync("example2.txt");
