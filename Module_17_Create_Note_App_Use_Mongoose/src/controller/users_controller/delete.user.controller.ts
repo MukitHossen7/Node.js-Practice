c6005d64c6ae61935308de3ffea59c8344b47348
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-15 19:50:14 +0600

    Add deleteUser functionality and integrate with user routes

---
+++ Module_17_Create_Note_App_Use_Mongoose/src/controller/users_controller/delete.user.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import User from "../../Models/user.model";
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const result = await User.findByIdAndDelete(id);
+    res.send(result);
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- Module_17_Create_Note_App_Use_Mongoose/src/routers/users.routes.ts
+++ Module_17_Create_Note_App_Use_Mongoose/src/routers/users.routes.ts
@@ -2,9 +2,12 @@ import express from "express";
 import { postUser } from "../controller/users_controller/post.user.controller";
 import { getUsers } from "../controller/users_controller/get.users.controller";
 import { updateUser } from "../controller/users_controller/update.user.controller";
+import { deleteUser } from "../controller/users_controller/delete.user.controller";
 
 export const usersRoutes = express.Router();
 
 usersRoutes.post("/", postUser);
 usersRoutes.get("/", getUsers);
 usersRoutes.patch("/:id", updateUser);
+usersRoutes.delete("/:id", deleteUser);
+// usersRoutes.delete("/:id", deleteUser);
