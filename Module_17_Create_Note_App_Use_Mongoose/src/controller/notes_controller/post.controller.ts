a5773859a3ff7caa09e6015667d6024f7d03757c
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-15 12:30:06 +0600

    Refactor postNotes controller to streamline note creation using Note.create method

--- Module_17_Create_Note_App_Use_Mongoose/src/controller/notes_controller/post.controller.ts
+++ Module_17_Create_Note_App_Use_Mongoose/src/controller/notes_controller/post.controller.ts
@@ -4,8 +4,10 @@ import Note from "../../Models/notes.post.model";
 export const postNotes = async (req: Request, res: Response) => {
   try {
     // const { title, content } = req.body;
-    const note = new Note(req.body);
-    const result = await note.save();
+    // const note = new Note(req.body);
+    // const result = await note.save();
+    const body = req.body;
+    const result = await Note.create(body);
     res.send(result);
   } catch (error) {
     console.error("Error in postNotes:", error);
