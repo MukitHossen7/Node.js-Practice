30613d42544cbd16a38a76e8da2adf18c92775cc
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-15 11:30:31 +0600

    Refactor note model to include detailed schema and update postNotes controller to accept full note object

--- Module_17_Create_Note_App_Use_Mongoose/src/Models/notes.post.model.ts
+++ Module_17_Create_Note_App_Use_Mongoose/src/Models/notes.post.model.ts
@@ -1,8 +1,55 @@
 import { model, Schema } from "mongoose";
 
 const noteSchema = new Schema({
-  title: String,
-  content: String,
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  tags: [
+    {
+      type: String,
+      default: [],
+    },
+  ],
+  metadata: {
+    createdBy: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+  },
+  tasks: [
+    {
+      task: {
+        type: String,
+        required: true,
+      },
+      assignedTo: {
+        type: String,
+      },
+      completed: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  ],
+  isImportant: {
+    type: Boolean,
+    default: false,
+  },
+  priority: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ["Work", "Personal", "Other"],
+    default: "Other",
+  },
 });
 
 const Note = model("Note", noteSchema);
--- Module_17_Create_Note_App_Use_Mongoose/src/controller/notes_controller/post.controller.ts
+++ Module_17_Create_Note_App_Use_Mongoose/src/controller/notes_controller/post.controller.ts
@@ -3,13 +3,10 @@ import Note from "../../Models/notes.post.model";
 
 export const postNotes = async (req: Request, res: Response) => {
   try {
-    const { title, content } = req.body;
-    const note = new Note({ title, content });
+    // const { title, content } = req.body;
+    const note = new Note(req.body);
     const result = await note.save();
     res.send(result);
-    // res.status(201).json({
-    //     message: "Note created successfully",
-    // });
   } catch (error) {
     console.error("Error in postNotes:", error);
     res.status(500).json({ message: "Internal Server Error" });
