0147ec285613e5240594414cae73e4ad3912fb8d
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-15 09:23:40 +0600

    Refactor postNotes controller to return saved note directly

--- Module_17_Create_Note_App_Use_Mongoose/src/controller/notes_controller/post.controller.ts
+++ Module_17_Create_Note_App_Use_Mongoose/src/controller/notes_controller/post.controller.ts
@@ -5,11 +5,11 @@ export const postNotes = async (req: Request, res: Response) => {
   try {
     const { title, content } = req.body;
     const note = new Note({ title, content });
-    await note.save();
-
-    res.status(201).json({
-      message: "Note created successfully",
-    });
+    const result = await note.save();
+    res.send(result);
+    // res.status(201).json({
+    //     message: "Note created successfully",
+    // });
   } catch (error) {
     console.error("Error in postNotes:", error);
     res.status(500).json({ message: "Internal Server Error" });
