20e006766cfe8ca88a5ab4231f76c07ffe79c1c3
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-17 00:55:01 +0600

    Refactor user creation to include Zod validation for request body

--- Module_17_Create_Note_App_Use_Mongoose/src/controller/users_controller/post.user.controller.ts
+++ Module_17_Create_Note_App_Use_Mongoose/src/controller/users_controller/post.user.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import User from "../../Models/user.model";
+import { UserZodSchema } from "../../zod.schema/user.zod.schema/user.zod.schema";
 
 export const postUser = async (req: Request, res: Response) => {
   try {
     const body = req.body;
-    const result = await User.create(body);
+    const data = await UserZodSchema.parse(body);
+    const result = await User.create(data);
     res.send(result);
   } catch (error) {
     console.error("Error creating user:", error);
