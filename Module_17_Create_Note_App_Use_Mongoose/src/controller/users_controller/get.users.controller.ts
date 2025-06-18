d628d3b22d17802fe1db539e90de0f4453ab5bef
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-18 12:43:00 +0600

    Update getUsers function to implement pagination with skip and limit

--- Module_17_Create_Note_App_Use_Mongoose/src/controller/users_controller/get.users.controller.ts
+++ Module_17_Create_Note_App_Use_Mongoose/src/controller/users_controller/get.users.controller.ts
@@ -3,7 +3,14 @@ import User from "../../Models/user.model";
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
-    const result = await User.find();
+    // let result;
+    // const { email } = req.query;
+    // if (email) {
+    //   result = await User.find({ email: email });
+    // } else {
+    //   result = await User.find();
+    // }
+    const result = await User.find().skip(5).limit(2);
     res.send(result);
   } catch (error) {
     console.error("Error fetching users:", error);
