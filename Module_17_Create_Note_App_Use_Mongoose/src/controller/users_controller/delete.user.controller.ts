e724c98257ffadfe1a6da34f0398f1b8fa06a0a0
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-18 10:55:28 +0600

    Refactor deleteUser function to use findByIdAndDelete for improved clarity

--- Module_17_Create_Note_App_Use_Mongoose/src/controller/users_controller/delete.user.controller.ts
+++ Module_17_Create_Note_App_Use_Mongoose/src/controller/users_controller/delete.user.controller.ts
@@ -4,8 +4,8 @@ import User from "../../Models/user.model";
 export const deleteUser = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    // const result = await User.findByIdAndDelete(id);
-    const result = await User.findOneAndDelete({ _id: id });
+    const result = await User.findByIdAndDelete(id);
+    // const result = await User.findOneAndDelete({ _id: id });
     res.send(result);
   } catch (error) {
     console.error("Error deleting user:", error);
